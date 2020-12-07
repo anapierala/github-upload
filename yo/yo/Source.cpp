b9d5b7f480fc8e7e8a30ebb1f84ab717d2b6e30b
Aaron Napierala <anapierala@defiance.edu>
2020-12-07 14:31:59 -0500

    yo

---
+++ yo/yo/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/************************************************************************
+* Author:	Aaron Napierala												*
+* Date:		5/30/2018													*
+*																		*
+* Exercise 1.1. Write a program that displays: Hello Universe!			*
+*																		*
+*************************************************************************/
+
+using namespace std;
+
+int main() {
+	// Following statements output string of characters that state Hello
+	// and then outputs two new lines!
+	cout << "Hello Universe! ";
+	cout << endl << endl;
+
+	// Pauses the console window awaiting user input to continue.
+	system("Pause");
+
+	return 0;
+}
